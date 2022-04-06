65fba5d6f33ca601f540c54d1c5bb5ddf0cdd228
Manum <moturi.maneesh19@st.niituniversity.in>
2022-04-06 19:06:41 +0530

    DONE --Splash Screen

--- app/src/main/java/com/example/budgetapp/MainActivity.java
+++ app/src/main/java/com/example/budgetapp/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.budgetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        }, SPLASH);
+
     }
 }
