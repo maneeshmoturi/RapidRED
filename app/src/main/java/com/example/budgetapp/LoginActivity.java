7a5789439738670c03087bdbb928a4f34897873d
Manum <moturi.maneesh19@st.niituniversity.in>
2022-04-06 23:09:53 +0530

    DONE --Basic Login Screen

--- app/src/main/java/com/example/budgetapp/LoginActivity.java
+++ app/src/main/java/com/example/budgetapp/LoginActivity.java
@@ -2,13 +2,36 @@ package com.example.budgetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmail, loginPassword;
+    private Button loginButton;
+    private TextView registerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
+        registerText = findViewById(R.id.registerText);
+
+        loginButton.setOnClickListener(view -> {
+            // TODO: onClick HomePageActivity
+        });
+
+        registerText.setOnClickListener(view -> {
+            // TODO: onClick RegisterActivity
+        });
+
     }
 }
--- app/src/main/java/com/example/budgetapp/MainActivity.java
+++ app/src/main/java/com/example/budgetapp/MainActivity.java
@@ -15,12 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(loginIntent);
-            }
+        new Handler().postDelayed(() -> {
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
         }, SPLASH);
 
     }
